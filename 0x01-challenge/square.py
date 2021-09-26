eea589acdafa2d5f8fa82d868114e0c0e1a5251d
M33x0 <michael.mbea@gmail.com>
2021-09-27 01:46:20 +0200

    madison square1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -24,6 +24,7 @@ class square():
         """String representation."""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
